b43cb69cb93f8260125a91d507fa154039d7818a
Jacobs David <jacobsdavid.dr@gmail.com>
2025-05-05 02:17:13 +0100

    Optimize hosted Images

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
+// next.config.js
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ["randomuser.me"],
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
